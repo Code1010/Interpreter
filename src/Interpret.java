e6b53a5b699cdda400a67303dbef246294f2e3c1
Cody Burrows <code1010@rochester.rr.com>
2015-02-02 15:00:52 -0500

    It works.
    Yes it does.

--- src/Interpret.java
+++ src/Interpret.java
@@ -16,24 +16,34 @@ public class Interpret {
 		File f = new File("C:/Users/Cody/Documents/Output.txt");
 		FileReader fr = new FileReader(f);
 		BufferedReader br = new BufferedReader(fr);
-		
-		String command = br.readLine();
-		int lines = 0;
+		String command = "default";
+		int lines = 1;
+		double x = 0;
+		double y = 0;
+		while(!command.equals("<<STOP>>")){
+			
+			command = br.readLine();
 				
-		if(!command.equals("")){
-			
-			if(command.equals("<<START>>")){
-				System.out.println(lines + ". We are at the starting position.");
-			} else if(command.contains("[MOVE]")){
-				//code for decoding x and y values
-			}
-			
-		} else {
-			
-			//done, celebrate!
+				if(command.equals("<<START>>")){
+					System.out.println(lines + ". We are at the starting position.");
+				} else if(command.contains("[MOVE]")){
+					//code for decoding x and y values
+				    x = Double.valueOf(command.substring(command.indexOf("X")+1, command.indexOf("Y")-1));
+				    y = Double.valueOf(command.substring(command.indexOf("Y")+1));
+					System.out.println(lines + ". Move " + x + " inches in x. Move " + y + " inches in Y.");
+				} else if(command.contains("[GATHER]")){
+					int totes;
+					String s = command.substring(command.indexOf((" "))).trim();
+					totes = Integer.valueOf(s);
+					System.out.println(lines + ". Gather " + totes + " tote(s)");
+				} else {
+					System.out.println("STOP");
+				}
+			lines++;
 			
 		}
 		
+		System.out.println("Reached the end of the file.");
 		
 		
 		fr.close();
